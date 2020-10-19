bd4d1a379e91def903b044802347d956514c69ba
boris <accscc@naver.com>
2020-10-19 02:49:13 +0000

    20.10.19

--- Project.cpp
+++ Project.cpp
@@ -1,1 +1,33 @@
-﻿
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+class LOG {
+	vector<string> v;
+public:
+	LOG& operator<<(const string& s) {
+		v.push_back(s);
+		time_t result = time(nullptr);
+		SYSTEMTIME curTime;	
+		GetLocalTime(&curTime);
+		cout << curTime.wHour << ":"
+			<< curTime.wMinute << ":"
+			<< curTime.wSecond << "     " << s << endl;
+		for (auto e : v) {
+			cout << e << endl;
+		}
+		return *this;
+	}
+};
+
+
+int main() {
+	LOG a;
+	a << "Hello, World!";
+	a << "Hello, World!";
+	a << "Hello, World!";
+	
+}
