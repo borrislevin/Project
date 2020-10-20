06420f241b0fe96d810b7bc7d3d28861e75394f0
boris <accscc@naver.com>
2020-10-20 01:41:57 +0000

    heading..

---
+++ Project_v0.1.cpp
@@ -0,0 +1,31 @@
+// 1. 메세지 전달은 못하겠다.
+// 2. LOG(INFO), LOG(WARNING) 만 사용하고 나머지 기능 구현하자 - ok
+// 3. 날짜 구현
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <Windows.h>
+#include <fstream>
+#include <filesystem>
+using namespace std;
+
+#define LOG(lvl) lvl::call(__func__,__LINE__)
+
+class Msg {
+	
+	virtual void call() = 0;
+	
+};
+
+class INFO :public Msg {
+	vector<string> v;
+public:
+	static void call(const char* func, int line) {
+		printf("%s:%d] INFO LOG", func, line);
+	}	
+};
+
+int main() {
+	LOG(INFO);
+}
