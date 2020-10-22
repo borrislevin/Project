6ac9b526a42f95ebe03b4df2f3e1ff6c44f33bda
boris <accscc@naver.com>
2020-10-22 01:00:30 +0000

    헤더 소스 분리 시작

--- VisualStudio/logging_v0.7.h
+++ VisualStudio/logging_v0.7.h
@@ -23,35 +23,93 @@ public:
 	Logger(const std::string& f) : filename(f) {	}
 
 	void write(const char* fmt, ...) {
+		std::ofstream out;
+		fs::create_directory(getDay());
+		std::string file;
+		int i = 0;
+		std::string temp;
+		while (1) {
+			temp = getDay() + "/" + filename + "_(" + std::to_string(i) + ").log";
+			if (GetFileSize(temp) < limit)
+				break;
+			else
+				i++;
+		}
+		file = temp;
+		out.open(file, std::ios::app);
+		if (!out.is_open()) {
+			std::cout << "로그 저장이 불가능합니다.";
+		}
+
+		std::string tempinfo = getLogLevel() + location + getTimestamp();
+
+		va_list start;
+		va_start(start, fmt);
+
+		char message[128];
+		vsprintf(message, fmt, start);
+		std::string s(message);
+		out << tempinfo << " " << message << std::endl;
+
+		out.close();
 	}
+
+
 	virtual std::string getLogLevel() const = 0;
 	virtual std::string getFilename() const = 0;
 
 	static Logger& getLogger(Level level) {
+		return *loggers[level];
 	}
 
-	Logger& setCurrentContext(const std::string& file, const std::string& func, int l) {	
+	Logger& setCurrentContext(const std::string& file, const std::string& func, int l) {	// 위치 정보
+		location = file + " " + func + "_" + "line:" + std::to_string(l) + " ";
+		cnt++;
+		return *this;
 	}
 
-	int GetFileSize(std::string s) {			
+	int GetFileSize(std::string) {			
 	}
 
 	void show(const std::string& message) {
+		std::cout << this->getLogLevel() << location << getTimestamp() << "] " << message << std::endl;
 	}
 
 	void showAll() {
+		for (int i = 0; i < cnt - 1; i++) {
+			std::cout << information[i] << std::endl;
+		}
 	}
 
 protected:
 	std::string getDay() const {
+		time_t current = time(nullptr);
+		struct tm* t = localtime(&current);
+		std::string day = std::to_string(t->tm_year + 1900) + "." +
+			std::to_string(t->tm_mon + 1) + "." +
+			std::to_string(t->tm_mday);
+		return day;
 	}
 
 	std::string getTime() const {
+		time_t current = time(nullptr);
+		struct tm* t = localtime(&current);
+		std::string ampm;
+		if (t->tm_hour < 12) ampm = "a.m";
+		else ampm = "p.m";
+		std::string time = getDay() + "_" +
+			std::to_string(t->tm_hour) + ampm;
+		return time;
 	}
 	std::string getTimestamp() const {
+		time_t current = time(nullptr);
+		struct tm* t = localtime(&current);
+		std::string time = getTime() + " " + std::to_string(t->tm_min) + ":" + std::to_string(t->tm_sec);
+		return time;
 	}
 
 	static void registLogger(Level level, Logger* logger) {
+		loggers[level] = logger;
 	}
 
 private:
@@ -70,15 +128,22 @@ private:
 class InfoLogger : public Logger {
 public:
 	InfoLogger() : Logger(getFilename()) {
+		Logger::registLogger(INFO, this);
 	}
 
 	std::string getLogLevel() const override {
+		return "INFO_";
 	}
 
 	std::string getFilename() const override {
+		std::string name;
+		name = "INFO_" + getTime();
+		return name;
 	}
 
 	static InfoLogger& getInstance() {
+		static InfoLogger instance;
+		return instance;
 	}
 
 private:
@@ -91,22 +156,28 @@ private:
 class WarningLogger : public Logger {
 public:
 	WarningLogger() : Logger(getFilename()) {
+		Logger::registLogger(WARNING, this);
 	}
 
 	std::string getLogLevel() const override {
+		return "WARNING_";
 	}
 
 	std::string getFilename() const override {
+		std::string name;
+		name = "WARNING_" + getTime();
+		return name;
 	}
 
 	static WarningLogger& getInstance() {
+		static WarningLogger instance;
+		return instance;
 	}
 
 private:
 	WarningLogger(const WarningLogger&) = delete;
 	void operator=(const WarningLogger&) = delete;
 };
-
 
 class ErrorLogger : public Logger {
 public:
@@ -115,9 +186,13 @@ public:
 	}
 
 	std::string getLogLevel() const override {
+		return "ERROR_";
 	}
 
 	std::string getFilename() const override {
+		std::string name;
+		name = "ERROR_" + getTime();
+		return name;
 	}
 
 	static ErrorLogger& getInstance() {
@@ -139,12 +214,18 @@ public:
 	}
 
 	std::string getLogLevel() const override {
+		return "FATAL_";
 	}
 
 	std::string getFilename() const override {
+		std::string fname;
+		fname = "FATAL_" + getTime();
+		return fname;
 	}
 
 	static FatalLogger& getInstance() {
+		static FatalLogger instance;
+		return instance;
 	}
 
 private:
