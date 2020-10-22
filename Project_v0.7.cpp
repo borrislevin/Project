6331d10788f5b4269f992001e8126ec3dc3e7558
boris <accscc@naver.com>
2020-10-22 04:31:37 +0000

    초안 완료

--- Project_v0.7.cpp
+++ Project_v0.7.cpp
@@ -1,117 +1,6 @@
-#include "logging_v0.7.h"
-
-class Logger {
-public:
-	virtual ~Logger() {}
-	Logger(const std::string& f) : filename(f) {	}
-
-	void write(const char* fmt, ...) {
-	}
-	virtual std::string getLogLevel() const = 0;
-	virtual std::string getFilename() const = 0;
-
-	static Logger& getLogger(Level level) {
-	}
-
-	Logger& setCurrentContext(const std::string& file, const std::string& func, int l) {
-	}
-
-	int GetFileSize(std::string s) {
-	}
-
-	void show(const std::string& message) {
-	}
-
-	void showAll() {
-	}
-
-protected:
-	std::string getDay() const {
-	}
-
-	std::string getTime() const {
-	}
-	std::string getTimestamp() const {
-	}
-
-	static void registLogger(Level level, Logger* logger) {
-	}
-};
-
-//Children
-//---------------------------------------------------------------------------------------
-
-class InfoLogger : public Logger {
-public:
-	InfoLogger() : Logger(getFilename()) {
-	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static InfoLogger& getInstance() {
-	}
-};
-
-
-
-class WarningLogger : public Logger {
-public:
-	WarningLogger() : Logger(getFilename()) {
-	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static WarningLogger& getInstance() {
-	}
-};
-
-
-class ErrorLogger : public Logger {
-public:
-	ErrorLogger() : Logger(getFilename()) {
-		Logger::registLogger(ERROR, this);
-	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static ErrorLogger& getInstance() {
-		static ErrorLogger instance;
-		return instance;
-	}
-};
-
-
-
-class FatalLogger : public Logger {
-public:
-	FatalLogger() : Logger(getFilename()) {
-		Logger::registLogger(FATAL, this);
-	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static FatalLogger& getInstance() {
-	}
-};
+#include"logging_v0.7.h"
 
 #define LOG(level) (Logger::getLogger(level).setCurrentContext(__FILE__,__func__, __LINE__))
-
 
 #define INITIALIZE_LOGGERS()				\
   do {										\
@@ -121,16 +10,159 @@ public:
 		FatalLogger::getInstance();			\
   } while (0)								\
 
-
 std::map<Level, Logger*> Logger::loggers;
 
+void Logger::write(const char* fmt, ...) {
+	std::ofstream out;
+	fs::create_directory(getDay());
+	std::string file;
+	int i = 0;
+	std::string temp;
+	while (1) {
+		temp = getDay() + "/" + filename + "_(" + std::to_string(i) + ").log";
+		if (getFileSize(temp) < limit)	break;
+		else	i++;
+	}
+	file = temp;
+	out.open(file, std::ios::app);
+	if (!out.is_open()) {
+		std::cout << "로그 저장이 불가능합니다.";
+	}
+	std::string tempinfo = getLogLevel()+"_" + location + getTimestamp();
+	va_list start;
+	va_start(start, fmt);
+	char message[128];
+	vsprintf(message, fmt, start);
+	std::string s(message);
+	out << tempinfo << " " << message << std::endl;
+	out.close();
+}
+
+Logger& Logger::getLogger(Level level) {
+	return *loggers[level];
+}
+
+Logger& Logger::setCurrentContext(const std::string& file, const std::string& func, int l) {	// 위치 정보
+	location = file + " " + func + "_" + "line:" + std::to_string(l) + "] ";
+	cnt++;
+	return *this;
+}
+int Logger::getFileSize(std::string s) {					// 파일 크기
+	std::ifstream in_file(s, std::ios::binary);
+	in_file.seekg(0, std::ios::end);
+	return in_file.tellg();
+}
+
+void Logger::show(const std::string& message) {
+	std::cout << this->getLogLevel() << location << getTimestamp() << "] " << message << std::endl;
+}
+void Logger::showLogToday() {
+	std::string s = getDay();
+	fs::path p(s);	
+	for (const fs::directory_entry& entry :
+		fs::directory_iterator(p)) {
+		std::cout << entry.path() << std::endl;		
+	}
+}
+void Logger::showLogOneday() {
+	std::cout << "날짜를 입력하세요(yyyy.mm.dd): ";
+	std::string p;
+	std::cin >> p;
+	if (!std::experimental::filesystem::exists(p))
+		std::cout << "폴더가 존재하지 않습니다!" << std::endl;
+	for (const fs::directory_entry& entry : 
+		fs::directory_iterator(p)) {
+		std::cout << entry.path() << std::endl;
+	}
+}
+
+std::string Logger::getDay() const {
+	time_t current = time(nullptr);
+	struct tm* t = localtime(&current);
+	std::string day = std::to_string(t->tm_year + 1900) + "." +
+		std::to_string(t->tm_mon + 1) + "." +
+		std::to_string(t->tm_mday);
+	return day;
+}
+std::string Logger::getTime() const {
+	time_t current = time(nullptr);
+	struct tm* t = localtime(&current);
+	std::string ampm;
+	if (t->tm_hour < 12) ampm = "a.m";
+	else ampm = "p.m";
+	std::string time = getDay() + "_" +
+		std::to_string(t->tm_hour) + ampm;
+	return time;
+}
+std::string Logger::getTimestamp() const {
+	time_t current = time(nullptr);
+	struct tm* t = localtime(&current);
+	std::string time = getTime() + " " + std::to_string(t->tm_min) + ":" + std::to_string(t->tm_sec);
+	return time;
+}
+void Logger::registLogger(Level level, Logger* logger) {
+	loggers[level] = logger;
+}
+std::string InfoLogger::getLogLevel() const{
+	return "INFO";
+}
+std::string InfoLogger::getFilename() const{
+	std::string name;
+	name = "INFO_" + getTime();
+	return name;
+}
+InfoLogger& InfoLogger::getInstance() {
+	static InfoLogger instance;
+	return instance;
+}
+
+std::string WarningLogger::getLogLevel() const  {
+	return "WARNING";
+}
+std::string WarningLogger::getFilename() const {
+	std::string name;
+	name = "WARNING_" + getTime();
+	return name;
+}
+WarningLogger& WarningLogger::getInstance() {
+	static WarningLogger instance;
+	return instance;
+}
+
+std::string ErrorLogger::getLogLevel() const {
+	return "ERROR";
+}
+std::string ErrorLogger::getFilename() const{
+	std::string name;
+	name = "ERROR_" + getTime();
+	return name;
+}
+ErrorLogger& ErrorLogger::getInstance() {
+	static ErrorLogger instance;
+	return instance;
+}
+
+std::string FatalLogger::getLogLevel() const {
+	return "FATAL";
+}
+std::string FatalLogger::getFilename() const {
+	std::string fname;
+	fname = "FATAL_" + getTime();
+	return fname;
+}
+FatalLogger& FatalLogger::getInstance() {
+	static FatalLogger instance;
+	return instance;
+}
 
 int main() {
 	int n = 10;	char c = 'A';
 	INITIALIZE_LOGGERS();
-	LOG(INFO).write("tetntn");
-	LOG(WARNING).write("tetntn");
-	LOG(ERROR).write("tetntn");
-	LOG(FATAL).write("tetntn");
-	LOG(INFO).write("hello - %d %c", n, c);
+	LOG(INFO).write("Project Presentation");
+	LOG(WARNING).write("Project Presentation");
+	LOG(ERROR).write("Project Presentation");
+	LOG(FATAL).write("Project Presentation");
+	LOG(INFO).write("Hello, World! - %d %c", n, c);
+	LOG(INFO).showLogToday();
+	LOG(INFO).showLogOneday();
 }
--- logging_v0.7.h
+++ logging_v0.7.h
@@ -9,6 +9,8 @@
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 
+
+
 enum Level {
 	INFO,
 	WARNING,
@@ -16,48 +18,29 @@ enum Level {
 	FATAL
 };
 
-
 class Logger {
 public:
 	virtual ~Logger() {}
 	Logger(const std::string& f) : filename(f) {	}
-
-	void write(const char* fmt, ...) {
-	}
+	void write(const char* fmt, ...);
 	virtual std::string getLogLevel() const = 0;
 	virtual std::string getFilename() const = 0;
-
-	static Logger& getLogger(Level level) {
-	}
-
-	Logger& setCurrentContext(const std::string& file, const std::string& func, int l) {	
-	}
-
-	int GetFileSize(std::string s) {			
-	}
-
-	void show(const std::string& message) {
-	}
-
-	void showAll() {
-	}
-
+	static Logger& getLogger(Level level);
+	Logger& setCurrentContext(const std::string&, const std::string&, int);
+	int getFileSize(std::string);
+	void show(const std::string&);
+	void showLogToday();
+	void showLogOneday();
+	
 protected:
-	std::string getDay() const {
-	}
-
-	std::string getTime() const {
-	}
-	std::string getTimestamp() const {
-	}
-
-	static void registLogger(Level level, Logger* logger) {
-	}
+	std::string getDay() const;
+	std::string getTime() const;
+	std::string getTimestamp() const;
+	static void registLogger(Level, Logger*);
 
 private:
 	std::string filename;
-	std::string location;
-	int filesize;
+	std::string location;	
 	int limit = 2000;
 	std::vector<std::string> information;
 	static std::map<Level, Logger*> loggers;
@@ -66,87 +49,53 @@ private:
 
 //Children
 //---------------------------------------------------------------------------------------
-
 class InfoLogger : public Logger {
 public:
 	InfoLogger() : Logger(getFilename()) {
+		Logger::registLogger(INFO, this);
 	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static InfoLogger& getInstance() {
-	}
-
+	std::string getLogLevel() const override;
+	std::string getFilename() const override;
+	static InfoLogger& getInstance();
 private:
 	InfoLogger(const InfoLogger&) = delete;
 	void operator=(const InfoLogger&) = delete;
 };
 
-
-
 class WarningLogger : public Logger {
 public:
 	WarningLogger() : Logger(getFilename()) {
+		Logger::registLogger(WARNING, this);
 	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static WarningLogger& getInstance() {
-	}
-
+	std::string getLogLevel() const override;
+	std::string getFilename() const override;
+	static WarningLogger& getInstance();
 private:
 	WarningLogger(const WarningLogger&) = delete;
 	void operator=(const WarningLogger&) = delete;
 };
-
 
 class ErrorLogger : public Logger {
 public:
 	ErrorLogger() : Logger(getFilename()) {
 		Logger::registLogger(ERROR, this);
 	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static ErrorLogger& getInstance() {
-		static ErrorLogger instance;
-		return instance;
-	}
-
+	std::string getLogLevel() const override;
+	std::string getFilename() const override;
+	static ErrorLogger& getInstance();
 private:
 	ErrorLogger(const ErrorLogger&) = delete;
 	void operator=(const ErrorLogger&) = delete;
 };
-
-
 
 class FatalLogger : public Logger {
 public:
 	FatalLogger() : Logger(getFilename()) {
 		Logger::registLogger(FATAL, this);
 	}
-
-	std::string getLogLevel() const override {
-	}
-
-	std::string getFilename() const override {
-	}
-
-	static FatalLogger& getInstance() {
-	}
-
+	std::string getLogLevel() const override;
+	std::string getFilename() const override;
+	static FatalLogger& getInstance();
 private:
 	FatalLogger(const FatalLogger&) = delete;
 	void operator=(const FatalLogger&) = delete;
